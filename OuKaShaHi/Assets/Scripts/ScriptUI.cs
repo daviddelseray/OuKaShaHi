536afaaa30a4cc20bba29f414c7ff5738292fa4a
daviddelseray <ddelseray@gmail.com>
2016-01-30 14:16:04 +0100

    Début UI Annonce

---
+++ OuKaShaHi/Assets/Scripts/ScriptUI.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ScriptUI : MonoBehaviour
+{
+    public Text m_Announcer;
+
+	// Use this for initialization
+	void Start ()
+    {
+        m_Announcer.enabled = false; 
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void DisplayText (string Attack)
+    {
+        m_Announcer.text = Attack;
+
+    }
+}
