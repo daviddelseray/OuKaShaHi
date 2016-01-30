1d50992fd471d68a5f3090fb4a5995b9901c0172
daviddelseray <ddelseray@gmail.com>
2016-01-30 21:25:16 +0100

    Intégration First Animations

---
+++ OuKaShaHi/Assets/Scripts/ScriptTest.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScriptTest : MonoBehaviour 
+{
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		this.transform.position = new Vector3 (this.transform.parent.transform.position.x, this.transform.position.y, this.transform.position.z);
+	}
+}
