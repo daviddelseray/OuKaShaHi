a1b727fa1c4f763216a515af77fccb6296aa8fd7
daviddelseray <ddelseray@gmail.com>
2016-01-30 10:50:49 +0100

    Création Séquences

--- OuKaShaHi/Assets/Scripts/ScriptGameManager.cs
+++ OuKaShaHi/Assets/Scripts/ScriptGameManager.cs
@@ -4,19 +4,55 @@ using System.Collections.Generic;
 
 public class ScriptGameManager : MonoBehaviour
 {
-   public int m_SequenceLength;
+  
+    //_______________________________________________________________________
+
+   //Texts Lists
+  
 
     public List<string> l_TextBank = new List<string>();
 
     List<string> l_Reading = new List<string>();
 
-    float m_TextBankLength;
+   //__________________________________________________________________________
 
-    int m_Index;
+   // Sound List
+   
 
     public List<AudioClip> l_SoundBank = new List<AudioClip>();
 
     List<AudioClip> l_Playing = new List<AudioClip>();
+
+
+    //__________________________________________________________________________
+
+    // Various Variables
+    float m_TextBankLength;
+
+    public int m_SequenceLength;
+
+    int m_Index;
+
+    int m_FillBankIndex;
+
+    int[] a_ExclusionArray = new int[4];
+
+    bool m_CheckIsGood;
+
+    int m_DifficultyLevel=1;
+
+    //_____________________________________________________________________________
+    // Levels of Difficulty
+    public int m_Difficulty1;
+    public int m_Difficulty2;
+    public int m_Difficulty3;
+    public int m_Difficulty4;
+
+
+
+
+    //_____________________________________________________________________________
+    //Components
 
     AudioSource as_Speaker;
     
@@ -37,7 +73,7 @@ public class ScriptGameManager : MonoBehaviour
 	    
 	}
 
-    void FillingSequence()
+    void FillingSequence()// Remplissage de la séquence, on prend dans une liste de stockage les textes des cris (affichages next) et les sons dans une autre et on les mets dans deux listes de lecture
     {
         for (int i = 0; i<m_SequenceLength; i++)
         {
@@ -51,7 +87,52 @@ public class ScriptGameManager : MonoBehaviour
         
     } 
 
-   IEnumerator ReadingSequence ()
+   /* void FillingBanks()
+    {
+        for (int k =0; k<4;k++)
+        {
+            m_FillBankIndex = Mathf.FloorToInt(Random.Range(0, l_TextsHere.Count));
+
+        }
+    }
+    */
+    /*void CheckingIndex()
+    {
+        for (int l = 0; l<=a_ExclusionArray.Length-1;l++)
+        {
+
+        }
+    }
+    */
+
+    void LaunchSequence ()
+    {
+        switch(m_DifficultyLevel)
+        {
+            case 1:
+                m_SequenceLength = m_Difficulty1;
+                break;
+
+            case 2:
+                m_SequenceLength = m_Difficulty2;
+                break;
+
+
+            case 3:
+                m_SequenceLength = m_Difficulty3;
+                break;
+
+
+            case 4:
+                m_SequenceLength = m_Difficulty4;
+                break;
+         }
+
+
+    }
+
+
+   IEnumerator ReadingSequence ()// Lecture de la séquence, on parcourt les listes de lecture pour afficher les textes et pour jouer les sons
     {
         for(int j=0; j <m_SequenceLength;j++)
         {
